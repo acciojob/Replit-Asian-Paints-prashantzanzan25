5a1a9af7f6bf15462a36c72d979bf591f2181e3d
Prashant Zanzan <139844406+prashantzanzan25@users.noreply.github.com>
2023-10-30 20:09:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+document.getElementById('change_button').addEventListener('click', function () {
+    const blockId = document.getElementById('block_id').value;
+    const color = document.getElementById('color_id').value;
+    const gridItem = document.getElementById(blockId);
+
+    if (gridItem) {
+        gridItem.style.backgroundColor = color;
+    }
+});
+
+document.getElementById('reset_button').addEventListener('click', function () {
+    const gridItems = document.querySelectorAll('.grid-item');
+
+    gridItems.forEach((item) => {
+        item.style.backgroundColor = 'transparent';
+    });
+});
